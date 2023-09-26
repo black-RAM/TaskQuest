c4c7c3bf0060215a31978264eb0998c85bd92854
black-RAM <pplanes311@gmail.com>
2023-09-26 13:37:06 +0300

    ToDo class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./style.scss";
+
+class ToDo {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.priority = priority;
+    this.due = dueDate;
+  }
+}
+
+const math = new ToDo("Math homework", "Exercise 11.1, Exercise 11.2, Exercise 11.3", "Thursday", "high");
+
+console.log(math)
