5505118469e5625dc4ba494727a100e3da33a416
black-RAM <pplanes311@gmail.com>
2023-09-28 11:55:00 +0300

    moved projects array, Project class, and ToDo class to app.js module

--- src/index.js
+++ src/index.js
@@ -1,28 +1,5 @@
 import "./style.scss";
-
-const projects = []
-
-class Project {
-  constructor(projectName) {
-    this.name = projectName;
-    this.todos = [];
-    projects.push(this)
-  }
-
-  addToDo(todo) {
-    this.todos.push(todo);
-  }
-}
-
-class ToDo {
-  constructor(title, description, dueDate, difficulty, priority) {
-    this.title = title;
-    this.description = description;
-    this.priority = priority;
-    this.difficulty = difficulty;
-    this.due = dueDate;
-  }
-}
+import { projects, Project, ToDo } from "./app";
 
 const math = new ToDo("Math homework", "Exercise 11.1, Exercise 11.2, Exercise 11.3", "Thursday", "hard", "high");
 
