38f0bb03ec262791c9da2bdfb5bcae5672f47d09
black-RAM <pplanes311@gmail.com>
2023-10-19 15:13:40 +0300

    renderToDo() inserts to-do article in DOM

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import "./buttons.js";
 import { projects, Project, ToDo } from "./app";
 
 const defaultProject = new Project("Default Project", [
-  new ToDo("Chores", "Wash the Dishes", new Date(2023, 8, 29), 3)
+  new ToDo("Chores", "Wash the Dishes", new Date(2023, 8, 29), 2),
+  new ToDo("Gym", "Deadlift 80kg", new Date(2023, 9, 10), 3)
 ]);
--- src/template.ts
+++ src/template.ts
@@ -18,6 +18,42 @@ function renderToDo(toDo: ToDo) {
   const detailsButton = document.createElement("button");
   const editButton = document.createElement("button");
   const deleteButton = document.createElement("button");
+  const dueDateP = document.createElement("p");
+
+  // attributes
+  checkBox.type = "checkbox";
+  checkBox.id = "completeCheck";
+  toDoTitle.htmlFor = "completeCheck";
+  dueDateP.classList.add("due-date");
+
+  editButton.type = "button";
+  deleteButton.type = "button";
+  detailsButton.type = "button";
+
+  editButton.title = "edit";
+  deleteButton.title = "delete";
+  detailsButton.classList.add("btn-outline-secondary");
+
+  detailsButton.innerText = "Details";
+  editButton.innerHTML = '<i class="bi bi-pencil-square"></i>';
+  deleteButton.innerHTML = '<i class="bi bi-trash"></i>';
+
+  // finally, adding content to the DOM
+  toDoTitle.innerText = toDo.title;
+
+  leftDiv.appendChild(checkBox)
+  leftDiv.appendChild(toDoTitle)
+
+  dueDateP.innerText = "1 Jan";
+  rightDiv.appendChild(dueDateP);
+  rightDiv.appendChild(detailsButton);
+  rightDiv.appendChild(editButton);
+  rightDiv.appendChild(deleteButton)
+
+  element.appendChild(leftDiv);
+  element.appendChild(rightDiv);
+
+  document.querySelector("section.to-do-page")?.appendChild(element);
 
 }
 
