a34b768228bcd1a439a5f91634e224b6d8d95968
black-RAM <pplanes311@gmail.com>
2023-11-16 21:33:21 +0300

    registered service worker with remote link to clear scope issues

--- src/sw.ts
+++ src/sw.ts
@@ -1,7 +1,7 @@
 // register PWA service worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    navigator.serviceWorker.register('https://black-ram.github.io/TaskQuest/service-worker.js').then(registration => {
       console.log('SW registered: ', registration);
     }).catch(registrationError => {
       console.log('SW registration failed: ', registrationError);
