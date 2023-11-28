b7e52319d67d696d77c662e4a08ca4a6ec457238
black-RAM <pplanes311@gmail.com>
2023-11-28 23:04:13 +0300

    file for project walkthrough

---
+++ src/welcome.ts
@@ -0,0 +1,13 @@
+import introJs from "intro.js";
+
+introJs().setOptions({
+  steps: [{
+    title: "Hello World :)",
+    intro: "Hey guys! Nice to meet you other modules."
+  },
+  {
+    title: "Great Expectations!",
+    intro: "I am really looking forward to creating your project walkthrough!"
+  }
+  ]
+}).start()
